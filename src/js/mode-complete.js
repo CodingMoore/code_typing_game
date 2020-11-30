4f922387d14f9caf3dc3945021527e2d2f886398
Randel M <Randel.C.Moore@gmail.com>
2020-11-30 15:08:21 -0800

    add additional TO-Dos
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>

--- src/js/mode-complete.js
+++ src/js/mode-complete.js
@@ -1,3 +1,5 @@
+import { timers } from "jquery";
+
 export class Complete {
   constructor() {
     this.userInput = [];
@@ -22,3 +24,23 @@ export class Complete {
   }
 }
 
+/*
+promptArray["this", "is", "an", "example"]
+display promptArary[i],
+newArray.push.(promptArray[i])
+newArray...["t","h","i","s"]
+
+$().text(newArray[0] + newArray[1] + newArray[2])
+
+1) time trial - patrick Nick
+2) add highlight incorrect - Tyler Randel
+3) percent correct
+4) chars per/min
+5) show question 2 out of 10
+6) add additional prompt arrays
+7) way to select prompt array
+
+
+game mode) fill in the blank
+
+*/
