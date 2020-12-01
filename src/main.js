f9cdd2238ceffc4c51a5d0ea3cc398345e63b494
Randel M <Randel.C.Moore@gmail.com>
2020-12-01 13:28:53 -0800

    clean/delete comments and console.log()s
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- src/main.js
+++ src/main.js
@@ -8,12 +8,9 @@ import { validater } from './js/validate-char.js';
 $(document).ready(function() {
   let game = new Complete();
   $("#promptOut").text(game.prompt1[0]);
-  // jquery event listener for any kind of change to the user input, calls the validater function:
-  // $('#inputField').on("input", ));
   $("#inputField").on("input", function() {
     $("#visual").html(validater($("#inputField").val(), game.prompt1[game.turnsTaken]));
   });
-
   $("form").submit(function(event) {
     event.preventDefault();
     game.userInput.push($("#inputField").val());
@@ -32,8 +29,5 @@ $(document).ready(function() {
         $("#incorrect").hide();
       } , 1000);
     }
-
   });
 });
-
-// validater($("#inputField").val(), game.prompt1[game.turnsTaken]
