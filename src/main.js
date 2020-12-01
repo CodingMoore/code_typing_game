e1008458db7f9f68c2f26c0484c56a1b6fa6cdf5
Nick Hennessy <njhnny@gmail.com>
2020-12-01 14:10:31 -0800

    add question counter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Complete } from './js/mode-complete.js';
 
+
 $(document).ready(function() {
   let game = new Complete();
   $("#promptOut").text(game.prompt1[0]);
